ca000575148e76f33d5c3c92f6801de8e3b4492f
Vikashdevdotio <vikas16kumar03@gmail.com>
2026-03-02 13:57:53 +0530

    Update API

--- Day-16/Backend/src/app.js
+++ Day-16/Backend/src/app.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     credentials:true,
-    origin:"http://localhost:5173"
+    origin:"https://instaclone-gmpf.onrender.com"
 }))
 app.use(express.static("./public"))
 
