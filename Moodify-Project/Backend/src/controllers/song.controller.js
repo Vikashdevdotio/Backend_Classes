09a93a19efbd2de989cdfdc0e4831248d6cb1c4e
Vikashdevdotio <vikas16kumar03@gmail.com>
2026-03-05 14:59:22 +0530

    feat: Add song upload functionality with multer and ImageKit integration

---
+++ Moodify-Project/Backend/src/controllers/song.controller.js
@@ -0,0 +1,82 @@
+const songModel = require("../models/song.model");
+const storageService = require("../services/storage.service");
+const id3 = require("node-id3");
+
+async function uploadSong(req, res) {
+  const songBuffer = req.file.buffer;
+  const { mood } = req.body;
+  const tags = id3.read(songBuffer);
+
+
+
+  /**
+   * Upload Song and Poster at a same time but when the both files are not uploaded then you don't move forward.
+   */
+  const [songFile, posterFile] = await Promise.all([
+    storageService.uploadFile({
+      buffer: songBuffer,
+      filename: tags.title + ".mp3",
+      folder: "/cohort-2/moodify/songs",
+    }),
+
+    storageService.uploadFile({
+      buffer: tags.image.imageBuffer,
+      filename: tags.title + ".jpeg",
+      folder: "/cohort-2/moodify/posters",
+    }),
+  ]);
+
+
+
+/**
+ *Firstly Upload songFile then upload posterFile 
+ */
+//   const songFile = await storageService.uploadFile({ 
+//     buffer: songBuffer,
+//     filename: tags.title + ".mp3",
+//     folder: "/cohort-2/moodify/songs",
+//   });
+
+/**
+ * Upload posterFile
+ */
+//   const posterFile = await storageService.uploadFile({
+//     buffer: tags.image.imageBuffer,
+//     filename: tags.title + ".jpeg",
+//     folder: "/cohort-2/moodify/posters",
+//   });
+
+  const song = await songModel.create({
+    title: tags.title,
+    url: songFile.url,
+    posterUrl: posterFile.url,
+    mood,
+  });
+
+  res.status(201).json({
+    message: "song created successfully",
+    song,
+  });
+}
+
+
+async function getSong(req,res){
+    const {mood} = req.query
+
+    const song = await songModel.findOne({
+        mood
+    })
+
+    res.status(200).json({
+        message:'Song fetched successfully.',
+        song,
+    })
+}
+
+
+
+
+module.exports = {
+  uploadSong,
+  getSong
+};
